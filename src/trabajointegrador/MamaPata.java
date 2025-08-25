30b4f57781d0d6a9b11159943a8a3b2f74112986
fiammadominguez-commits <fiammadominguez@sanluis.edu.ar>
2025-08-25 19:39:44 -0300

    mama patito

--- src/trabajointegrador/MamaPata.java
+++ src/trabajointegrador/MamaPata.java
@@ -3,8 +3,8 @@ package trabajointegrador;
 
 
 public interface MamaPata {
-   public final double PICO_TAMAÑO = 6.8;
-   public final double PATA_TAMAÑO = 7.5;
+   public final double PICO_LARGO = 6.8;
+   public final double PATA_LARGO = 7.5;
    
    public void tocarOrgano();
     
